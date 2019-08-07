b4b6197a774c9ad746def35370a380c56916c7be
ECWireless <Elliott@valtechcreative.com>
2019-08-07 16:28:44 -0400

    App frame is complete

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 
 function App() {
-  return (
-    <div className="App">
-      StudioME 2.0 Coming Soon!
-    </div>
-  );
+	return (
+		<div className="container">
+			<div className="header">
+				<div className="header__menu">Menu</div>
+				<div className="header__logo">Logo</div>
+				<div className="header__cart">Cart</div>
+				<div className="header__profile">Profile</div>
+			</div>
+			<div className="sidebar"></div>
+			<div className="main">Main Content</div>
+			<div className="footer"></div>
+		</div>
+	);
 }
 
 export default App;
