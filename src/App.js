0b9171c52ea2ab9c86163f481d23443f62ce4574
ECWireless <Elliott@valtechcreative.com>
2019-08-07 14:46:23 -0400

    Create React App

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      StudioME 2.0 Coming Soon!
+    </div>
+  );
+}
+
+export default App;
