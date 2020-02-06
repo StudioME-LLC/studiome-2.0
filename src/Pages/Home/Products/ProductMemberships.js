80c793cd371427971647774e72e7129acc970f87
StudioME - Left iMac <studiome-leftimac@StudioME-Left.fios-router.home>
2020-02-06 14:13:29 -0500

    Starting to add memberships

--- src/Pages/Home/Products/ProductMemberships.js
+++ src/Pages/Home/Products/ProductMemberships.js
@@ -15,15 +15,18 @@ export default function ProductMemberships() {
                 >here
             </a> to sign up for our free membership.</p>
             
-            <p className="home__products-memberships-paragraph">Paid memberships are coming soon! Help us decide how memberships should look by filling out the survey below.</p>
-            <a
-                className="home__products-memberships-survey-link"
-                target="_blank"
+            <p className="home__products-memberships-paragraph">For paid memberships, we offer two simple plans:</p>
+
+            <div className="home__products-memberships-package home__products-memberships-packag--1">
+                <p className="home__products-memberships-package-name">$25mo / 25%off</p>
+            </div>
+
+            <p className="home__products-memberships-feedback">Want to give us feedback on our membership plans? <a
                 href="https://forms.gle/9Xor9rmRvPSFWhRA8"
-                rel="noopener noreferrer"
-            >
-                <button className="home__products-memberships-survey">Take the survey!</button>
-            </a>
+                rel="noopener noreferrer" target="_blank">
+                    Click Here
+                </a>
+            </p>
         </div>
     )
 }
